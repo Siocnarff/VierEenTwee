ec588cfc9a3677b748a56ef06e25c9d079b1d727
LOUISVA2000 <55919278+LOUISVA2000@users.noreply.github.com>
2020-10-28 15:59:57 +0200

    Add files via upload

---
+++ src/rce/simulator/SimulatorHotCondition.h
@@ -0,0 +1,13 @@
+#ifndef SIMULATORHOTCONDITION_H
+#define SIMULATORHOTCONDITION_H
+
+#include "Simulator.h"
+
+class SimulatorHotCondition :public Simulator 
+{
+
+public:
+	SimulatorHotCondition();
+	void SimulateWeather();
+};
+#endif
---
+++ src/rce/simulator/SimulatorNormalCondition.h
@@ -0,0 +1,13 @@
+#ifndef SIMULATORNORMALCONDITION_H
+#define SIMULATORNORMALCONDITION_H
+
+#include "Simulator.h"
+
+class SimulatorNormalCondition :public Simulator 
+{
+public:
+	SimulatorNormalCondition();
+	void SimulateWeather();
+};
+
+#endif
