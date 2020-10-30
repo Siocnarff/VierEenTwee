8445dea4dbdbb61849ee0e775c7d6772b2acc720
jo-anne <u17091820@tuks.co.za>
2020-10-30 11:05:53 +0200

    fix: namespace rce added

--- src/rce/RacingDep.h
+++ src/rce/RacingDep.h
@@ -23,47 +23,58 @@
 
 #include <string>
 #include <list>
-class RacingDep 
-{
 
-private:
-	int risklevel;
-	int budget;
-	Race* race;
-	int results;
-	CreateStrategy* strategy;
-	log::Container* CarContainer;
-	std::string TeamName;
-	list<ppl::Strategist*> Stategist;
-	list<ppl::Pitcrew*> pitcrew;
-	eng::Car* car;
-	
+namespace rce {
+    class RacingDep {
+
+    private:
+        int risklevel;
+        int budget;
+        Race *race;
+        int results;
+        CreateStrategy *strategy;
+        log::Container *CarContainer;
+        std::string TeamName;
+        list<ppl::Strategist *> Stategist;
+        list<ppl::Pitcrew *> pitcrew;
+        eng::Car *car;
+
 // / ppl::Driver array of size 2?
 // / car array of size 2? 
 // / different strategies for each ppl::Driver and car?
-public:
-	void HireEmployees(int b);
-	CreateStrategy* PlanSeasonStrategy(int budget,string weather,int riskLevel);//weather
-	void trainDriver(string weather, ppl::Driver* driver,int trackDifficulty,int time);//weather and time 
-	void preRaceArrival(eng::Car* c, ppl::Driver* d, Race* r, Container* con);
-	int RacingWeekend();// why is it an int?
-	void postRacePackUp();// return the container with tires in
-	
-	void registerForSeason(Observer* logisticsDept);// is it needed?
-	
-	Leaderboard* getResults();
-	void setResult(int result);
-	int getResult();// needed?
+    public:
+        void HireEmployees(int b);
 
-	Race* getRace();
-	string getTeamName();
-	void setTeamName(string TeamName);
-	void SetCarAfterRace();
+        CreateStrategy *PlanSeasonStrategy(int budget, string weather, int riskLevel);//weather
+        void trainDriver(string weather, ppl::Driver *driver, int trackDifficulty, int time);//weather and time
+        void preRaceArrival(eng::Car *c, ppl::Driver *d, Race *r, Container *con);
 
-	list<ppl::Person*> getStategist();
-	void setStategist(list<ppl::Person*> Stategist);
-	list<ppl::Person*> getPitcrew();
-	void setPitcrew(list<ppl::Person*> pitcrew);
-};
+        int RacingWeekend();// why is it an int?
+        void postRacePackUp();// return the container with tires in
 
+        void registerForSeason(Observer *logisticsDept);// is it needed?
+
+        Leaderboard *getResults();
+
+        void setResult(int result);
+
+        int getResult();// needed?
+
+        Race *getRace();
+
+        string getTeamName();
+
+        void setTeamName(string TeamName);
+
+        void SetCarAfterRace();
+
+        list<ppl::Person *> getStategist();
+
+        void setStategist(list<ppl::Person *> Stategist);
+
+        list<ppl::Person *> getPitcrew();
+
+        void setPitcrew(list<ppl::Person *> pitcrew);
+    };
+}
 #endif
