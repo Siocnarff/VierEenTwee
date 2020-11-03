8357fa8214e16c6015d1d9dd093411fe0dd40227
Gianni <gianni.volpi.sa@gmail.com>
2020-11-03 08:32:11 +0200

    Removed debugging in specialistsImproveComponent.

--- src/eng/componentDevelopment/Department.cpp
+++ src/eng/componentDevelopment/Department.cpp
@@ -103,13 +103,8 @@ void Department::specialistsImproveComponent(Component *component) {
     }
     double average = double(totalSkill) / double(teamSize);
     double result = int(average * 0.2 + best * 0.4 + riskLevel->trySomethingNew() * 0.4);
-	std::cout << "Average:" << average << std::endl;
-	std::cout << "Best:" << best << std::endl;
-	std::cout << "Result:" << result << std::endl;
     double percentage = ((100 - component->quality) / 100.0 > 0.05) ? 0.05 : (100 - component->quality) / 200.0;
-    std::cout << "Percentage:" << percentage << std::endl;
     component->quality += (int) (result * percentage);
-    std::cout << "-----------------------Here is the component quality after the improvement was made:" << component->quality << std::endl;
 }
 
 Department::~Department() {
